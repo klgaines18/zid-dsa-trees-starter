412a0b16c4569dd0942948d5cac0a7dd3022af3f
Katie Gaines <klgaines18@gmail.com>
2023-04-07 05:50:02 -0400

    add dfs in order

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -148,4 +148,22 @@ class BinarySearchTree {
     }
     return this.left._findMin();
   }
+
+
+  dfsInOrder(values = []) {
+    // First, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsInOrder(values);
+    }
+
+    // Next, process the current node
+    values.push(this.value);
+
+    // Finally, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsInOrder(values);
+    }
+
+    return values;
+  }
 }
